7f1c5358f57dad27d757c1d736648c5b7adb7d03
milespucarelli <miles.pucarelli@gmail.com>
2019-02-28 00:17:15 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,133 @@
 import React, { Component } from 'react';
 import './stylesheets/App.css'
 import { Segment } from 'semantic-ui-react';
+import WestworldMap from './components/WestworldMap'
+import Headquarters from  './components/Headquarters'
 
 
 class App extends Component {
-
   // As you go through the components given you'll see a lot of functional components.
   // But feel free to change them to whatever you want.
   // It's up to you whether they should be stateful or not.
 
+  state = {
+    areas: [],
+    hosts: [],
+    current: 'Details',
+    logs: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:4000/areas')
+      .then(res => res.json())
+      .then(areas => this.setState({areas}))
+    fetch('http://localhost:4000/hosts')
+      .then(res => res.json())
+      .then(hosts => this.setState({hosts}))
+  }
+
+  clickHandler = (host) => {
+    this.setState({current: host})
+  }
+
+  changeHandler = (movedHost, newArea) => {
+    let area = this.state.areas.find(area => area.name === newArea)
+    let hosts = this.state.hosts.filter(host => host.area === area.name && host.active)
+    if (area.limit >= hosts.length + 1 || !movedHost.active) {
+      let newHosts = this.state.hosts.map(host => {
+        if (host.id === movedHost.id) {
+          host.area = newArea
+        }
+        return host
+      })
+      this.setState({
+        hosts: newHosts,
+        logs: [...this.state.logs,
+          {
+            type: 'notify',
+            message: `${movedHost.firstName} set in area ${newArea.split('_').map(string => {
+              return string.charAt(0).toUpperCase() + string.slice(1)
+            }).join(' ')}`
+          }]
+      })
+    } else {
+      this.setState({
+        logs: [
+          {
+            type: 'error',
+            message: `Too many hosts. Cannot add ${movedHost.firstName} to ${newArea}`
+          }, ...this.state.logs]
+      })
+    }
+  }
+
+  toggleHandler = (activatedHost) => {
+    let area = this.state.areas.find(area => area.name === activatedHost.area)
+    let hosts = this.state.hosts.filter(host => host.area === area.name && host.active)
+    if (area.limit >= hosts.length + 1) {
+      let newHosts = this.state.hosts.map(host => {
+        if (host.id === activatedHost.id) {
+          host.active = !host.active
+        }
+        return host
+      })
+      this.setState({
+        hosts: newHosts,
+        logs: [
+          {
+            type: 'warn',
+            message: `Activated ${activatedHost.firstName}`
+          }, ...this.state.logs]
+      })
+    } else {
+      this.setState({
+        logs: [
+          {
+            type: 'error',
+            message: `Too many hosts. Cannot add ${activatedHost.firstName} to ${activatedHost.area}`
+          }, ...this.state.logs]
+      })
+    }
+  }
+
+  toggleAllHandler = (clicked) => {
+    let newHosts = this.state.hosts.map(host => {
+      if (!clicked && !host.active) {
+        host.active = true
+      } else if (clicked && host.active) {
+        host.active = false
+      }
+      return host
+    })
+
+    this.setState({
+      hosts: newHosts,
+      logs: [
+        {
+          type: clicked ? 'notify' : 'warn',
+          message: clicked ? 'Decommissiong all hosts.' : 'Activating all hosts!'
+        }, ...this.state.logs]
+    })
+  }
+
+
   render(){
     return (
       <Segment id='app'>
-        {/* What components should go here? Check out Checkpoint 1 of the Readme if you're confused */}
+        <WestworldMap
+          areas={this.state.areas}
+          hosts={this.state.hosts}
+          current={this.state.current}
+          clickHandler={this.clickHandler}/>
+        <Headquarters
+          areas={this.state.areas}
+          hosts={this.state.hosts}
+          current={this.state.current}
+          logs={this.state.logs}
+          clickHandler={this.clickHandler}
+          changeHandler={this.changeHandler}
+          toggleHandler={this.toggleHandler}
+          toggleAllHandler={this.toggleAllHandler}/>
       </Segment>
     )
   }
--- src/components/LogPanel.js
+++ src/components/LogPanel.js
@@ -2,39 +2,60 @@ import React from 'react'
 import { Segment, Button } from 'semantic-ui-react';
 import { Log } from '../services/Log'
 
-const LogPanel = () => {
+class LogPanel extends React.Component {
 
-  const dummyLogs = () => {
-    // This is just to show you how this should work. But where should the log data actually get stored?
-    // And where should we be creating logs in the first place?
-    // Use the Log Service class (located in: 'src/services/Log') we've created anywhere you like.
-    // Just remember to import it
-
-    let logs = []
-
-    logs.unshift(Log.warn("This is an example of a warn log"))
-    logs.unshift(Log.notify("This is an example of a notify log"))
-    logs.unshift(Log.error("This is an example of an error log"))
-
-    return logs
+  // const dummyLogs = () => {
+  //   // This is just to show you how this should work. But where should the log data actually get stored?
+  //   // And where should we be creating logs in the first place?
+  //   // Use the Log Service class (located in: 'src/services/Log') we've created anywhere you like.
+  //   // Just remember to import it
+  //
+  //   let logs = []
+  //
+  //   logs.unshift(Log.warn("This is an example of a warn log"))
+  //   logs.unshift(Log.notify("This is an example of a notify log"))
+  //   logs.unshift(Log.error("This is an example of an error log"))
+  //
+  //   return logs
+  // }
+  /* This isn't always going to be the same color...*/
+  /* Should the button always read "ACTIVATE ALL"? When should it read "DECOMMISSION ALL"? */
+  state = {
+    clicked: false
   }
 
-  return(
-    <Segment className="HQComps" id="logPanel">
-      <pre>
-        {dummyLogs().map((log, i) => <p key={i} className={log.type}>{log.msg}</p>)}
-      </pre>
-      
-      {/* Button below is the Activate All/Decommisssion All button */}
-      <Button
-        fluid
-        color={"red"}
-        {/* This isn't always going to be the same color...*/}
-        content={"ACTIVATE ALL"}
-        {/* Should the button always read "ACTIVATE ALL"? When should it read "DECOMMISSION ALL"? */}
-      />
-    </Segment>
-  )
+  logs = () => this.props.logs.map((log, i) => {
+    if (log.type === 'notify') {
+      return <p key={i} className={log.type}>{Log.notify(log.message).msg}</p>
+    } else if (log.type === 'warn') {
+      return <p key={i} className={log.type}>{Log.warn(log.message).msg}</p>
+    } else {
+      return <p key={i} className={log.type}>{Log.error(log.message).msg}</p>
+    }
+  })
+
+  clickHandler = () => {
+    this.props.toggleAllHandler(this.state.clicked)
+    this.setState({clicked: !this.state.clicked})
+  }
+
+  render() {
+    return (
+      <Segment className="HQComps" id="logPanel">
+        <pre>
+          {this.logs()}
+        </pre>
+
+        {/* Button below is the Activate All/Decommisssion All button */}
+        <Button
+          fluid
+          color={this.state.clicked ? "green" : "red"}
+          content={this.state.clicked ? "DECOMMISSION ALL" : "ACTIVATE ALL"}
+          onClick={this.clickHandler}
+        />
+      </Segment>
+    )
+  }
 }
 
 export default LogPanel
